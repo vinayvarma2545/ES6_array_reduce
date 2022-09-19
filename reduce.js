88f72ec855aaf793c12c1340deb2f39c5310d450
vinay <vinayvarma2545@gmail.com>
2022-09-19 20:59:47 +0530

    reduce

---
+++ reduce.js
@@ -0,0 +1,13 @@
+// const array1 = [1,2,3,4,5];
+// const initialValue = 0;
+// const sumWithInitial = array1.reduce(
+//   (previousValue, currentValue) => previousValue + currentValue,
+//   initialValue
+// );
+
+// console.log(sumWithInitial);
+
+
+let box1 = [1,2,3,4,5];
+let ans = box1.reduce((a,b)=>a*b);
+console.log(ans)
